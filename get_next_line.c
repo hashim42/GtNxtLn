e486804315cf212340d87332f2e8b43d49ffabee
hashim42 <aazzout@hotmail.com>
2017-01-15 20:11:42 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hazzout <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/29 10:23:53 by hazzout           #+#    #+#             */
-/*   Updated: 2017/01/04 02:11:14 by hazzout          ###   ########.fr       */
+/*   Updated: 2017/01/10 02:11:56 by hazzout          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,19 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-t_fd_buff	*get_curr_buff(t_fd_list **fd_list, int fd)
+t_fd_buff	*get_curr_buff(t_list **fd_list, int fd)
 {
 	t_fd_buff 	*curr_fd_buff;
-	size_t		size;
+	t_list	*tmp;
+	t_fd_buff   *curr;
 
-	while (*fd_list)
+	tmp = *fd_list;
+	while (tmp)
 	{
-		curr_fd_buff = (t_fd_buff*)((*fd_list)->content);
+		curr_fd_buff = (t_fd_buff*)((tmp)->content);
 		if (curr_fd_buff->fd == fd)
 			return (curr_fd_buff);
-		*fd_list = (*fd_list)->next;
+		tmp = tmp->next;
 	}
 	curr_fd_buff = (t_fd_buff*)malloc(sizeof(t_fd_buff));
 	if (curr_fd_buff)
@@ -33,10 +35,12 @@ t_fd_buff	*get_curr_buff(t_fd_list **fd_list, int fd)
 		curr_fd_buff->lines = ft_strnew(BUFF_SIZE);
 		if (!(curr_fd_buff->lines))
 			return (NULL);
-		curr_fd_buff->filend = 0;
-		curr_fd_buff->len = 0;
-		size = sizeof(curr_fd_buff);
-		ft_lstadd(fd_list, ft_lstnew(curr_fd_buff, size));
+		tmp = ft_lstnew(curr_fd_buff, sizeof(curr_fd_buff));
+		curr = (t_fd_buff*)(tmp->content);
+		curr->fd = fd;
+		ft_lstadd(fd_list, tmp);
+		ft_memdel((void **)&curr_fd_buff);
+		curr_fd_buff = (t_fd_buff*)((*fd_list)->content);
 	}
 	return (curr_fd_buff);
 }
@@ -47,7 +51,7 @@ int			set_fd_buff_lines(t_fd_buff *curr_fd_buff, char *buff)
 
 	tmp = curr_fd_buff->lines;
 	curr_fd_buff->lines = ft_strjoin(tmp, buff);
-	ft_strdel(&tmp);
+	//ft_strdel(&tmp);
 	return (ft_strichr(buff, 10));
 }
 
@@ -59,19 +63,22 @@ int			read_from_file(t_fd_buff *curr_fd_buff)
 
 	if (curr_fd_buff->filend == 1)
 		return (0);
-	pos = -1;
 	buff = ft_strnew(BUFF_SIZE);
-	while ((ret = read(curr_fd_buff->fd, buff, BUFF_SIZE)) > 0 && pos < 0)
+	if ((ret = read(curr_fd_buff->fd, buff, BUFF_SIZE)) < 0)
+		return (-1);
+	buff[ret] = '\0';
+	pos = set_fd_buff_lines(curr_fd_buff, buff);
+	while (ret > 0 && pos < 0)
 	{
-		
+
+		ret = read(curr_fd_buff->fd, buff, BUFF_SIZE);
 		buff[ret] = '\0';
 		pos = set_fd_buff_lines(curr_fd_buff, buff);
 	}
-	if (pos >= 0 && ret > 0)
-		set_fd_buff_lines(curr_fd_buff, buff);
 	curr_fd_buff->filend = (ret == 0) ? 1 : 0;
 	curr_fd_buff->len = ft_strlen(curr_fd_buff->lines);
-	return (1);
+	ft_strdel(&buff);
+	return (ret < 0 ? ret : 1);
 }
 
 int			read_fd_buff(t_fd_buff *curr_fd_buff, char **line)
@@ -81,16 +88,17 @@ int			read_fd_buff(t_fd_buff *curr_fd_buff, char **line)
 	char	*newline;
 	char	*nextline;
 
+
 	if (ft_strichr(curr_fd_buff->lines, 10) < 0)
-		read_from_file(curr_fd_buff);
-	ft_putstr(curr_fd_buff->lines);
+		if(read_from_file(curr_fd_buff) < 0)
+			return (-1);
 	tmp = curr_fd_buff->lines;
 	nextline = ft_strchr(curr_fd_buff->lines, 10);
 	if (nextline)
 		curr_fd_buff->lines = nextline + 1;
 	pos = nextline ? nextline - tmp : ft_strlen(curr_fd_buff->lines);
+	//ft_strdel(&tmp);
 	newline = ft_strsub(tmp, 0, pos);
-	ft_strdel(&tmp);
 	tmp = *line;
 	*line = newline;
 	ft_strdel(&tmp);
@@ -99,16 +107,22 @@ int			read_fd_buff(t_fd_buff *curr_fd_buff, char **line)
 
 int			get_next_line(const int fd, char **line)
 {
-	static t_fd_list	*fd_list;
+	static t_list	*fd_list;
 	t_fd_buff			*curr_fd_buff;
 	int					ret;
 
+	if (fd < 0)
+		return (-1);
 	curr_fd_buff = get_curr_buff(&fd_list, fd);
 	if (!curr_fd_buff)
 		return (-1);
 	if ((ret = read_fd_buff(curr_fd_buff, line)) == 1)
 		return (ret);
+	if (ret < 0)
+		return (ret);
 	if (ret == 0 && curr_fd_buff->filend == 1)
 		return (0);
 	return (1);
 }
+
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hazzout <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/29 17:21:29 by hazzout           #+#    #+#             */
-/*   Updated: 2017/01/06 12:55:00 by hazzout          ###   ########.fr       */
+/*   Updated: 2017/01/10 01:44:30 by hazzout          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ typedef struct		s_fd_buff
 	int					len;
 }					t_fd_buff;
 
-typedef t_list t_fd_list;
+//typedef t_list t_fd_list;
 
-# define BUFF_SIZE 10
+# define BUFF_SIZE 1
 
 int		get_next_line(const int fd, char **line);
 
